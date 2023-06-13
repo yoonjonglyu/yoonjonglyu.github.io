11eb87c8d12ccf5a42fb6855299d684d8aad740d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-06-13 23:42:58 +0900

    feat: 패키지 PAGE INIT

---
+++ app/package/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'package',
+  description: 'package',
+};
+
+export default function Page () {
+  return <h1>Hello, Next.js!</h1>;
+}
