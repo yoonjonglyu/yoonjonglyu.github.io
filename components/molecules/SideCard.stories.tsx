27585b29b2073050b6e4e40be079cf3a66ff2763
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-07-09 21:23:46 +0900

    feat(MOLECULE): SIDE CARD - 해더 컨텐츠 기본 스타일링

--- components/molecules/SideCard.stories.tsx
+++ components/molecules/SideCard.stories.tsx
@@ -14,5 +14,19 @@ type Story = StoryObj<typeof SideCard>;
 
 export const Basic: Story = {
   args: {
+    header: (
+      <>
+        <h3>카테고리 목록</h3>
+      </>
+    ),
+    contents: (
+      <ul>
+        <li>
+          <a>주제1</a>
+        </li>
+        <li>주제2</li>
+        <li>주제3</li>
+      </ul>
+    ),
   },
 };
--- components/molecules/SideCard.tsx
+++ components/molecules/SideCard.tsx
@@ -4,10 +4,37 @@ import styled from 'styled-components';
 
 import Card from '../atoms/Card';
 
-export interface SideCardProps {}
+const CardHeader = styled.div`
+  height: 30px;
+  border-bottom: 1px dashed var(--color-background-space);
+  h3 {
+    font-size: 14px;
+    font-weight: 500;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+`;
+const CardContents = styled.div`
+  font-size: 12px;
+  a:hover {
+    text-decoration: underline;
+    color: var(--color-point);
+  }
+`;
 
-const SideCard: React.FC<SideCardProps> = (props) => {
-  return <Card></Card>;
+export interface SideCardProps {
+  header?: React.ReactNode;
+  contents?: React.ReactNode;
+}
+
+const SideCard: React.FC<SideCardProps> = ({ header, contents }) => {
+  return (
+    <Card>
+      <CardHeader>{header}</CardHeader>
+      <CardContents>{contents}</CardContents>
+    </Card>
+  );
 };
 
 export default SideCard;
