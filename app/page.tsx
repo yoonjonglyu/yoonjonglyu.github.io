b64aa0c6b34a18dbd2badebdf200cbede342163b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-07-16 06:53:43 +0900

    feat(APP): ROOT PAGE - 기본적인 구성 및 스타일링

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+'use client';
+import styled from 'styled-components';
+
+import SideCard from '../components/molecules/SideCard';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 18px;
+  margin: 24px 3px;
+  @media (max-width: 1024px) {
+    flex-direction: column-reverse;
+  }
+`;
+const ContentsArea = styled.section`
+  flex: 3;
+  overflow: hidden;
+`;
+const SideArea = styled.aside`
+  flex: 1;
+  overflow: hidden;
+`;
+
 export default function RootPage() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <Container>
+      <ContentsArea>asd</ContentsArea>
+      <SideArea>
+        <SideCard header={<h3>사이드바</h3>} />
+      </SideArea>
+    </Container>
+  );
 }
