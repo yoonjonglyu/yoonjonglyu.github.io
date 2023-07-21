d79ce63ad7f7db740a996d0b40500ee9152b4a5d
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-07-21 11:05:37 +0900

    feat(FEATURE): HOME SIDE - MOB X SIDE STYLE

--- features/home/HomeSide.tsx
+++ features/home/HomeSide.tsx
@@ -5,7 +5,27 @@ import SideCard from '../../components/molecules/SideCard';
 
 const SideArea = styled.aside`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
   overflow: hidden;
+  overflow-x: auto;
+
+  &::-webkit-scrollbar {
+    height: 3px;
+    background: none;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: var(--color-point);
+    border-radius: 12px;
+  }
+
+  @media (max-width: 1024px) {
+    flex-direction: row;
+    & div {
+      flex: 0 0 95%;
+    }
+  }
 `;
 
 export interface HomeSideProps {}
@@ -13,7 +33,58 @@ export interface HomeSideProps {}
 const HomeSide: React.FC<HomeSideProps> = () => {
   return (
     <SideArea>
-      <SideCard header={<h3>사이드바</h3>} />
+      <SideCard
+        header={<h3>사이드바</h3>}
+        contents={
+          <ul>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+          </ul>
+        }
+      />
+      <SideCard
+        header={<h3>사이드바</h3>}
+        contents={
+          <ol>
+            <li>
+              리스트업 목록
+              <ol>
+                <li>리스트업 목록2</li>
+                <li>리스트업 목록2</li>
+                <li>리스트업 목록2</li>
+                <li>리스트업 목록2</li>
+              </ol>
+            </li>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+          </ol>
+        }
+      />
+      <SideCard
+        header={<h3>사이드바</h3>}
+        contents={
+          <ol>
+            <li>
+              리스트업 목록
+              <ol>
+                <li>리스트업 목록2</li>
+                <li>리스트업 목록2</li>
+                <li>리스트업 목록2</li>
+                <li>리스트업 목록2</li>
+              </ol>
+            </li>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+            <li>리스트업 목록</li>
+          </ol>
+        }
+      />
     </SideArea>
   );
 };
