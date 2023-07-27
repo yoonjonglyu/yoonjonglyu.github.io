679ac9aa7160785bbb667fb4a263a6486deec856
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-07-27 21:43:10 +0900

    feat(FEATURE): HOME BANNER - 구성 및 스타일링

--- features/home/HomeBanner.tsx
+++ features/home/HomeBanner.tsx
@@ -3,26 +3,55 @@ import styled, { keyframes } from 'styled-components';
 
 import Card from '../../components/atoms/Card';
 
+const TextBox = styled.div`
+  position: absolute;
+  top: 25px;
+  right: 28px;
+  text-align: right;
+  z-index: 9999;
+`;
 const Graffiti = keyframes`
-0 {
-  text-shadow: 2px 2px 2px tomato, 0 0 1em blue, 0 0 0.2em red;
+0% {
+  text-shadow: 1px 1px 1px var(--color-legend), 0 0 1em blue, 0 0 0.2em var(--color-rare);
+}
+25% {
+  text-shadow: 0.8px 0.8px 1px var(--color-point), 0 0 1em blue, 0 0 0.2em blue;
+}
+35% {
+  text-shadow: 2px 2px 2px var(--color-point), 0 0 1em blue, 0 0 0.2em blue;
 }
 50% {
-  text-shadow: 0.5px 0.5px 2px orange, 0 0 1em blue, 0 0 0.2em blue;
+  text-shadow: 0.5px 0.5px 2px var(--color-point), 0 0 1em blue, 0 0 0.2em blue;
+}
+75% {
+  text-shadow: 1px 1px 1px var(--color-unique), 0 0 1em blue, 0 0 0.2em blue;
 }
 100% {
-  text-shadow: 2px 2px 2px red, 0 0 1em blue, 0 0 0.2em blue;
+  text-shadow: 1.5px 1.5px 1.5px var(--color-unique), 0 0 1em blue, 0 0 0.2em var(--color-legend);
 }
 `;
-const GraffitiText = styled.div`
+const GraffitiText = styled.p`
+  margin: 3px 0;
   font-size: 2rem;
-  animation: ${Graffiti} 1s infinite;
+  animation: ${Graffiti} 3s infinite;
 `;
-const Ice = styled.div`
+const Moon = styled.div`
+  position: absolute;
+  top: 25px;
   width: 200px;
   height: 200px;
-  background: blue;
   border-radius: 100%;
+  background: linear-gradient(35deg, var(--color-unique), var(--color-point));
+  z-index: 999;
+`;
+const Star = styled.div`
+  position: absolute;
+  top: 230px;
+  width: 125px;
+  height: 125px;
+  border-radius: 100%;
+  background: linear-gradient(35deg, var(--color-legend), var(--color-point));
+  z-index: 999;
 `;
 
 export interface HomeBannerProps {}
@@ -32,11 +61,18 @@ const HomeBanner: React.FC<HomeBannerProps> = () => {
     <Card
       css={`
         min-height: 400px;
+        background: linear-gradient(
+          55deg,
+          var(--color-point),
+          var(--color-legend)
+        );
       `}>
-      <GraffitiText>Welcome to My Archive.</GraffitiText>
-      <GraffitiText>...</GraffitiText>
-      <GraffitiText>...</GraffitiText>
-      <Ice />
+      <TextBox>
+        <GraffitiText>Welcome to My Archive</GraffitiText>
+        <GraffitiText>from. Devloper ISA</GraffitiText>
+      </TextBox>
+      <Moon />
+      <Star />
     </Card>
   );
 };
