8538e00315405175fcfe8b59db892f8c5bea41dd
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-08-02 22:44:29 +0900

    feat(PAGE): PACKAGE - FEATURE BIND

--- app/package/page.tsx
+++ app/package/page.tsx
@@ -1,10 +1,30 @@
+'use client';
 import { Metadata } from 'next';
+import styled from 'styled-components';
+
+import PackageContents from '../../features/package/PackageContents';
+import PackageSide from '../../features/package/PackageSide';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 18px;
+  margin: 24px 3px;
+  @media (max-width: 1024px) {
+    flex-direction: column-reverse;
+  }
+`;
 
 export const metadata: Metadata = {
   title: 'package',
   description: 'package',
 };
 
-export default function PackagePage () {
-  return <h1>Hello, package</h1>;
+export default function PackagePage() {
+  return (
+    <Container>
+      <PackageContents />
+      <PackageSide />
+    </Container>
+  );
 }
