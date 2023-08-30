5147e62b4591ca488f77d5d76b60bbdbac00f7e7
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-08-31 06:02:58 +0900

    test(스토리북): 글로벌 CSS IMPORT 설정 변경

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { Preview } from '@storybook/react';
 
-import GlobalCSS from '../provider/style/GlobalCSS';
+import { GlobalCSS } from '../provider/style/GlobalCSS';
 
 const preview: Preview = {
   parameters: {
