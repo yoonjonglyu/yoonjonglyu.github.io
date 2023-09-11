addaf2546fee13c5a24e4f672a5afea94b0cb20a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-09-11 20:46:15 +0900

    feat(HOOKS): USE SNIPPETLIST - API BIND

--- hooks/snippet/useSnippetList.ts
+++ hooks/snippet/useSnippetList.ts
@@ -1,12 +1,15 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 
 import { SnippetListState } from '../../store/snippet';
+
+import { GetSnippetList } from '../../api/snippet';
 
 const useSnippetList = () => {
   const [snippetList, setSnippetList] = useRecoilState(SnippetListState);
 
   const updateSnippetList = async () => {
-    setSnippetList([]);
+    const res = await GetSnippetList();
+    setSnippetList(res || []);
   };
 
   return { snippetList, updateSnippetList };
