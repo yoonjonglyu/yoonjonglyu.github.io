28241292c17a151410a79deb28d3c7134c86ad28
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-10-04 07:36:09 +0900

    feat: ADD META TAG

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -3,8 +3,18 @@ import { Metadata } from 'next';
 import ProjectList from '../../features/project/ProjectList';
 
 export const metadata: Metadata = {
-  title: 'project',
-  description: 'project',
+  title: 'ISA Archive - Project',
+  description: 'about project',
+  keywords: [
+    'devlop',
+    'ISA',
+    'frontend',
+    'archive',
+    '개발',
+    '프론트엔드',
+    'react',
+    'project',
+  ],
 };
 
 export default function ProjectPage() {
--- app/snippet/page.tsx
+++ app/snippet/page.tsx
@@ -3,8 +3,18 @@ import { Metadata } from 'next';
 import SnippetContents from '../../features/snippet/SnippetContents';
 
 export const metadata: Metadata = {
-  title: 'snippet',
-  description: 'snippet',
+  title: 'ISA Archive - Snippet',
+  description: 'about snippet',
+  keywords: [
+    'devlop',
+    'ISA',
+    'frontend',
+    'archive',
+    '개발',
+    '프론트엔드',
+    'react',
+    'snippet',
+  ]
 };
 
 export default function SnippetPage() {
