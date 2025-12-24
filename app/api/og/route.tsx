45355c227218967358cfea63b76e19fc2384c177
Isa(yunjongryu) <yunjonglyu@gmail.com>
2025-12-25 04:44:40 +0900

    chore: CICD - MDX & BUG FIX

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from 'next/og';
 
-export const runtime = 'chrome';
+export const runtime = 'edge';
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
